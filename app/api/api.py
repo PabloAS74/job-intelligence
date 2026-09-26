a56df2dccb4912cefe90dad9ef5229194bd65ea2
PabloAS74 <andressalgadopablo@gmail.com>
2026-09-26 13:28:51 +0200

    added job_id endpoint

--- app/api/api.py
+++ app/api/api.py
@@ -1,9 +1,9 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app.db.database import SessionLocal, get_db
 from app.db.models import Job, Role, Location
-from app.schemas.response_schemas import JobResponse
+from app.schemas.response_schemas import JobResponse, JobDetailedResponse
 
 # Inicializamos la API  
 app = FastAPI(
@@ -36,3 +36,17 @@ def get_jobs(
     ofertas = query.limit(limit).all()
 
     return ofertas
+
+
+@app.get("/api/jobs/{job_id}", response_model=JobDetailedResponse)
+def get_job_by_id(job_id: int, db: Session = Depends(get_db)):
+    """
+    Devuelve los detalles de una oferta de trabajo por su ID.
+    """
+    job = db.query(Job).filter(Job.id == job_id).first()
+    
+    # Si no existe lanzamos error 404 limpio
+    if not job:
+        raise HTTPException(status_code=404, detail="Job not found")
+    
+    return job
--- app/schemas/response_schemas.py
+++ app/schemas/response_schemas.py
@@ -3,29 +3,54 @@ from datetime import datetime
 
 from app.db.models import RemoteType, EmploymentType, ExperienceLevel
 
+class CompanyResponse(BaseModel):
+    id: int
+    name: str
+    website: str | None = None
+    model_config = {"from_attributes": True}
+
+class LocationResponse(BaseModel):
+    id: int
+    city: str
+    model_config = {"from_attributes": True}
+
+class RoleResponse(BaseModel):
+    id: int
+    name: str
+    model_config = {"from_attributes": True}
+
+
 class JobResponse(BaseModel):
         id: int
         title: str
-        description: str | None
-    
-        role_id: int
-        company_id: int
-        location_id: int
+         
+        role: RoleResponse
+        company: CompanyResponse
+        location: LocationResponse
     
         remote_type: RemoteType | None = None
         employment_type: EmploymentType | None = None
-        experience_level: ExperienceLevel | None = None
         
         salary_min: int | None = None
         salary_max: int | None = None
         salary_currency: str | None = None  # Ej: EUR, USD
     
         published_at: datetime | None = None
+    
+        model_config = {"from_attributes": True}
+        
+        
+class JobDetailedResponse(JobResponse):
+        """
+        Extiendo JobResponse para incluir información adicional pesada.
+        """
+        description: str | None = None
+        experience_level: ExperienceLevel | None = None
+        
         first_seen_at: datetime 
         last_seen_at: datetime 
-    
+        
         source: str # Ej: "linkedin", "glassdoor"
         source_id: str # El ID original de la oferta en esa web
         url: str
-        
-        model_config = {"from_attributes": True}
+                
